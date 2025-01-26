515f333d5490c9ef57ff991482768b74c476cc74
Nelson Manuel <nelsonmanuel1125@gmail.com>
2025-01-26 15:06:23 -0500

    first commit

---
+++ src/main/java/co/com/bancolombia/pruebasubida/PruebasubidaApplication.java
@@ -0,0 +1,13 @@
+package co.com.bancolombia.pruebasubida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebasubidaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebasubidaApplication.class, args);
+    }
+
+}
